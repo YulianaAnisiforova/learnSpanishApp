8a97e79cf7930b3a1a84b5953d179c9b243a4b32
Woonbeam Post <yulianaanisiforova@gmail.com>
2025-04-21 14:04:05 +0300

    changing theme title in progress

--- src/components/list/Wordlist.tsx
+++ src/components/list/Wordlist.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import style from './Wordlist.module.css'
 import {useDispatch, useSelector} from 'react-redux'
 import {AppStateType} from '../../redux/store'
@@ -12,6 +12,8 @@ const Wordlist = () => {
     const cards = useSelector((state: AppStateType) => state.cardsPage.cards)
     const themes = useSelector((state: AppStateType) => state.cardsPage.themes)
     const dispatch = useDispatch()
+
+    const [isEditing, setIsEditing] = useState(false)
 
     const groupedCards = themes.map(theme => ({
         theme,
@@ -30,26 +32,41 @@ const Wordlist = () => {
         dispatch(cardsActions.deleteListAC(theme))
     }
 
+    const activateEditMode = () => {
+        setIsEditing(true)
+    }
+
+    const deactivateEditMode = () => {
+        setIsEditing(false)
+    }
+
     return (
         <div className={style.wordlistBox}>
             {groupedCards.map(group => (
                 <div key={group.theme}>
-                    <div className={style.themeTitle}>{group.theme}
-                        <CopyOutlined className={style.copyIcon}
-                        onClick={() => onCopyClick(group.theme)} />
-                        <DeleteOutlined className={style.deleteListIcon}
-                                        onClick={() => onDeleteListClick(group.theme)} />
+                    <div className={style.titleBox}>
+                        {isEditing
+                            ? <input className={style.changeTitle} onBlur={deactivateEditMode} autoFocus/>
+                            : <div className={style.themeTitle}
+                                   onDoubleClick={activateEditMode}>
+                                {group.theme}
+                                <CopyOutlined className={style.copyIcon}
+                                              onClick={() => onCopyClick(group.theme)}/>
+                                <DeleteOutlined className={style.deleteListIcon}
+                                                onClick={() => onDeleteListClick(group.theme)}/>
+                            </div>
+                        }
                     </div>
-                        <div className={style.listBox}>
-                            {group.cards.map(card =>
-                                <WordlistItem key={card.cardID}
-                                              cardID={card.cardID}
-                                              cardTheme={card.cardTheme}
-                                              cardWord={card.cardWord}
-                                              cardTranslate={card.cardTranslate}
-                                              onDeleteClick={onDeleteClick}
-                                />)}
-                        </div>
+                    <div className={style.listBox}>
+                        {group.cards.map(card =>
+                            <WordlistItem key={card.cardID}
+                                          cardID={card.cardID}
+                                          cardTheme={card.cardTheme}
+                                          cardWord={card.cardWord}
+                                          cardTranslate={card.cardTranslate}
+                                          onDeleteClick={onDeleteClick}
+                            />)}
+                    </div>
                 </div>
             ))}
 
